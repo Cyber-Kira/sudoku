c40dc7907296529ce884dc5f2956f033d5a53a53
Kirill Koloskov <kirillkoloskovm@gmail.com>
2021-08-06 19:17:20 +0300

    feat: add sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+function possible(matrix, y, x, n) {
+  for (let i = 0; i < 9; i++) {
+    if (matrix[y][i] === n || matrix[i][x] === n) {
+      return false;
+    }
+  }
+
+  const xSquare = Math.floor(x / 3) * 3;
+  const ySquare = Math.floor(y / 3) * 3;
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (matrix[ySquare + i][xSquare + j] === n) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
+
+module.exports = function solveSudoku(matrix) {
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
+      if (matrix[y][x] === 0) {
+        for (let n = 1; n <= 9; n++) {
+          if (possible(matrix, y, x, n)) {
+            matrix[y][x] = n;
+
+            if (solveSudoku(matrix)) return matrix;
+          }
+        }
+
+        matrix[y][x] = 0;
+        return false;
+      }
+    }
+  }
+
+  return matrix;
+};
